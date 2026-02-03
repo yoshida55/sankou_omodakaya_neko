d547b5bf1f33b1a2c231a9968331ef5af9f99883
kenzo <sensha5172+copilot2@gmail.com>
2026-02-03 11:22:33 +0900

    aaa

--- js/work.js
+++ js/work.js
@@ -5,9 +5,12 @@ window.addEventListener("scroll", function () {
 
   // store_img要素を取得
   const storeImg = document.querySelector(".store_img");
+  const manekiNekoImg = document.querySelector(".left_cat_img");
 
   // 要素の位置を取得(Bounding・・・境界　RectはRectangle・・・座標・大きさ)
   const rect = storeImg.getBoundingClientRect();
+  // 要素の位置を取得(招き猫)
+  const rectManekiNeko = manekiNekoImg.getBoundingClientRect();
 
   // ウィンドウ（表示領域）の高さを取得
   const windowHeight = window.innerHeight;
@@ -16,5 +19,11 @@ window.addEventListener("scroll", function () {
   if (rect.top <= windowHeight) {
     // アニメーションを開始
     storeImg.classList.add("animate");
+  }
+
+  // 要素の上端がウィンドウの下端に到達したか確認(招き猫)
+  if (rectManekiNeko.top <= windowHeight) {
+    // アニメーションを開始
+    manekiNekoImg.classList.add("animate");
   }
 });
